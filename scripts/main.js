7c5e54cc222de05296a03cb78d0f4efaab1231c6
avocodev <avocodev@gmail.com>
2024-12-16 11:54:19 +0700

    Make features for ongoing to completed task

--- scripts/main.js
+++ scripts/main.js
@@ -23,11 +23,11 @@ buttonElement.addEventListener('click', () => {
 function renderPage() {
   let todoHTML = '';
 
-  taskTodo.forEach((value) => {
+  taskTodo.forEach((value, index) => {
     todoHTML += `
       <li>
         <p>${value['task-name']}</p>
-        <button class="done-button">Selesai</button>
+        <button class="done-button js-done-button" data-button-id="${index}">Selesai</button>
       </li>
     `;
   });
@@ -36,3 +36,39 @@ function renderPage() {
 }
 
 renderPage();
+
+const completedTask = [];
+const activeButton = document.querySelectorAll('.js-done-button');
+
+function completedRenderPage() {
+  let todoHTML = '';
+
+  completedTask.forEach((value, index) => {
+    todoHTML += `
+      <li>
+        <p>${value['task-name']}</p>
+        <button class="delete-button js-delete-button" data-button-id="${index}">Hapus</button>
+      </li>
+    `;
+    
+    document.querySelector('.list-task-compeleted').innerHTML = todoHTML;
+  });
+
+  console.log(todoHTML);
+}
+
+activeButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const { buttonId } = button.dataset;
+    const taskCompleted = taskTodo[buttonId];
+
+    taskTodo.splice(buttonId, 1);
+
+    taskCompleted.status = true;
+    completedTask.push(taskCompleted);
+
+    completedRenderPage();
+
+    renderPage();
+  });
+});
